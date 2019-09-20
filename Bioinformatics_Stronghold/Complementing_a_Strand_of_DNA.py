1fb778eb353edcf3088601e5264acfac49f726cd
priyamraut <praut9@gatech.edu>
2019-09-19 21:06:36 -0400

    spelling error

--- Bioinformatics_Stronghold/Complementing_a_Strand_of_DNA.py
+++ Bioinformatics_Stronghold/Complementing_a_Strand_of_DNA.py
@@ -1,20 +1,20 @@
 #usr/bin/python3
 DNA_string='AAAACCCGGT'
 DNA_string=input("Input the input string")
-Reverse_Compliment=''
+Reverse_Complement=''
 if len(DNA_string) >1000:
     print("Warning: base pair length greater than 1000; continuing.")
 for every_character in DNA_string:
     if every_character =='A':
-        Reverse_Compliment='T'+Reverse_Compliment
+        Reverse_Complement='T'+Reverse_Complement
     elif every_character =='G':
-        Reverse_Compliment='C'+Reverse_Compliment
+        Reverse_Complement='C'+Reverse_Complement
     elif every_character =='C':
-        Reverse_Compliment='G'+Reverse_Compliment
+        Reverse_Complement='G'+Reverse_Complement
     elif every_character =='T':
-        Reverse_Compliment='A'+Reverse_Compliment
+        Reverse_Complement='A'+Reverse_Complement
     else:
         print("Nucleotide other than AGCT")
 print("Reverse Compliment is ")
-print(Reverse_Compliment)
-print(len(Reverse_Compliment))
+print(Reverse_Complement)
+print(len(Reverse_Complement))
