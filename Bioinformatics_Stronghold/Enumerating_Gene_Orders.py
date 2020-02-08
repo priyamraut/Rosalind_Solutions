78eceb6ee6a92f2f5ff96e871deb1654f03d0907
priyamraut <praut9@gatech.edu>
2020-02-08 01:10:36 -0500

    Update : Enumerating Gene Orders

--- Bioinformatics_Stronghold/Enumerating_Gene_Orders.py
+++ Bioinformatics_Stronghold/Enumerating_Gene_Orders.py
@@ -1,7 +1,7 @@
 print("A permutation of length n is an ordering of the positive integers {1,2,…,n}. For example, π=(5,3,2,1,4) is a permutation of length 5.\
 Given: A positive integer n≤7.Return: The total number of permutations of length n, followed by a list of all such permutations (in any order).")
 
-from collections import permutations 
+from itertools import permutations 
 
 x=int(input("Enter the length of the permutation\n"))
 def factorial(number):
@@ -10,8 +10,14 @@ def factorial(number):
         fact=fact*i
     return fact
 total_number_of_permutations=factorial(x)
-print(total_number_of_permutations)
-
-for i in range(0,total_number_of_permutations):
-    
-
+f = open('output.txt','w')
+print(total_number_of_permutations,file=f)
+list_perm=[]
+for i in range(1,x+1):
+    list_perm.append(i)
+perm = permutations(list_perm)
+for every_tuple in perm:
+    for every_element in every_tuple:
+        print(every_element,end=" ",file=f)
+    print(end="\n",file=f)
+f.close()   
