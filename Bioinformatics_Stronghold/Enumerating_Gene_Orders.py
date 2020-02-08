300973340c660fa94ffb39ea519700235585db51
priyamraut <praut9@gatech.edu>
2020-02-07 23:44:42 -0500

    Update : Enumerating Gene Orders

---
+++ Bioinformatics_Stronghold/Enumerating_Gene_Orders.py
@@ -0,0 +1,8 @@
+print("A permutation of length n is an ordering of the positive integers {1,2,…,n}. For example, π=(5,3,2,1,4) is a permutation of length 5.\
+Given: A positive integer n≤7.Return: The total number of permutations of length n, followed by a list of all such permutations (in any order).")
+
+x=int(input("Enter the length of the permutation"))
+
+for i in range(0,x):
+    print(i)
+
